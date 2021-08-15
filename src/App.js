b60f3a1173e95fcee1ab63ddfecd7f704a72818c
Cedric Quenette <cedricquenette@outlook.com>
2021-08-16 07:04:18 +1000

    5.2: bloglist frontend, step2

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUser = window.localStorage.getItem('loggedInUser')
+    if (loggedInUser) {
+      const user = JSON.parse(loggedInUser)
+      setUser(user)
+    }
+  }, [])
+
   const loginForm = () => (
     <div>
       <h2>log in to application</h2>
@@ -50,6 +58,9 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -58,12 +69,22 @@ const App = () => {
     }
   }
 
+  const logout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+  }
+
   const showBlogs = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
-        {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+        <div>
+          <p>{user.name} logged in</p>
+          <button onClick={() => {logout()}}>logout</button>
+        </div>
+        <div>
+          {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+        </div>
       </div>
     )
   }
